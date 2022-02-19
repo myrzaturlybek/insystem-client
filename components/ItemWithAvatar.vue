5f6f60ec21498ee8f0964313f02e24d49abf27ef
Myrza <myrza.turlybek01@gmail.com>
2022-02-19 16:23:40 +0600

    feat: add component item-with-avatar

---
+++ components/ItemWithAvatar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="d-flex justify-content-between">
+    <div class="d-flex">
+      <div class="ava">
+        <img :src="imgUrl" alt="ava" :class="{ circle: avaCircle }" />
+      </div>
+      <div class="ml16 d-flex flex-column justify-content-between">
+        <slot name="leftColumn"></slot>
+      </div>
+    </div>
+    <div class="d-flex flex-column justify-content-between align-items-end">
+      <slot name="rightColumn"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: {
+      type: String,
+      default: '',
+    },
+    avaCircle: Boolean,
+  },
+}
+</script>
+
+<style scoped>
+.ava img {
+  width: 48px;
+  height: 48px;
+  border-radius: 10px;
+}
+
+.circle {
+  border-radius: 50%;
+}
+</style>
---
+++ components/Order/ItemInOrder.vue
@@ -0,0 +1,47 @@
+<template>
+  <item-with-avatar :img-url="imgUrl">
+    <template #leftColumn>
+      <p class="bold-text">{{ price.toLocaleString('ru') }} {{ currency }}</p>
+      <p>{{ description }}</p>
+    </template>
+    <template #rightColumn>
+      <p class="bold-text">{{ number }} {{ unit }}</p>
+      <p class="little-grey-text">{{ suplier }}</p>
+    </template>
+  </item-with-avatar>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: {
+      type: String,
+      default: '/iphone-11.jpg',
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    currency: {
+      type: String,
+      default: 'тг',
+    },
+    description: {
+      type: String,
+      default: 'Смартфон Apple iPhone X 256Gb Space Gray (MQAF2RJBJB)',
+    },
+    number: {
+      type: Number,
+      default: 0,
+    },
+    unit: {
+      type: String,
+      default: 'шт',
+    },
+    suplier: {
+      type: String,
+      default: 'Mobilka electronik',
+    },
+  },
+}
+</script>
